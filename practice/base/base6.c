7239f93ed38481457cee5aafa72af21b6feb013e
Artur Barilo <faustkeller45gmail.com>
2019-01-25 00:13:48 +0300

    master | improve factorial calculation

---
+++ practice/base/base6.c
@@ -0,0 +1,30 @@
+/*
+ * Created by efreyu on 1/24/19.
+ * Вычисление факториала
+ * Factorial calculation
+ */
+#include <stdio.h>
+
+long int fact(int n);
+
+int main(int argc, const char *argv[]) {
+
+    int n;
+    printf("Enter your number:");
+    scanf("%d", &n);
+    if (n > 0) {
+        printf("The factorial of %d is %ld\n", n, fact(n));
+    } else {
+        printf("Number cannot be less than zero or zero\n");
+    }
+
+    return 0;
+}
+
+long int fact(int n) {
+    if (n >= 1) {
+        return n * fact(n - 1);
+    } else {
+        return 1;
+    }
+}
