80ea75827aa7381c52fdd48b236f4ba0a23b0a23
Artur Barilo <faustkeller45gmail.com>
2019-01-28 13:48:26 +0300

    algorithm_3 | hw3

--- algorithm/lesson3/hw/hw1.c
+++ algorithm/lesson3/hw/hw1.c
@@ -7,42 +7,73 @@
 
 #include <stdio.h>
 
-void bubble_sort(int list[], int len, int * count);
+int bubble_sort_one(int arr[], int len);
+
+int bubble_sort_two(int arr[], int len);
 
 int main(int argc, const char *argv[]) {
-    int array[100], len, i, counter = 0;
+    int len = 10, i,
+            counter_one = 0,
+            counter_two = 0;
 
-    printf("Введите количество элементов массива\n");
-    scanf("%d", &len);
+    int array1[100] = {9,8,7,6,5,4,3,2,1,0};
 
-    printf("Вы указали %d, теперь введите его значения\n", len);
+    counter_one = bubble_sort_one(array1, len);
+    int array2[100] = {9,8,7,6,5,4,3,2,1,0};
 
-    for (i = 0; i < len; i++)
-        scanf("%d", &array[i]);
+    counter_two = bubble_sort_two(array2, len);
 
-    bubble_sort(array, len, &counter);
-
-    printf("Отсортированный массив содержит:\n");
-
-    for (i = 0; i < len; i++)
-        printf("%d\n", array[i]);
-
-    printf("Было выполненно %d операций(операции)\n", counter);
+    printf("В bubble_sort_one было выполненно %d операций(операции)\n", counter_one);
+    printf("В bubble_sort_two было выполненно %d операций(операции)\n", counter_two);
 
     return 0;
 }
 
-void bubble_sort(int list[], int len, int * count) {
+void bubble_sort(int arr[], int len, int *count) {
     int i, j, temp;
 
     for (i = 0; i < len - 1; i++) {
         for (j = 0; j < len - i - 1; j++) {
-            if (list[j] > list[j + 1]) {
+            if (arr[j] > arr[j + 1]) {
                 *count = *count + 1;
-                temp = list[j];
-                list[j] = list[j + 1];
-                list[j + 1] = temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
+}
+
+int bubble_sort_one(int arr[], int len) {
+    int i, j, temp, count = 0;
+
+    for (i = 0; i < len - 1; i++) {
+        for (j = 0; j < len - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                count = count + 1;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    return count;
+}
+
+int bubble_sort_two(int arr[], int len) {
+    int i, j, temp, count = 0;
+
+    for (i = len - 1; i >= 0; i--) {
+        for (j = 0; j < i; j++) {
+            if (arr[j + 1] < arr[j]) {
+                count = count + 1;
+                temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    return count;
 }
