45fa0f0ecd9c4b8a0d44aee36dd4f60b5c72227f
efreyu <faustkeller45@gmail.com>
2019-01-31 09:23:43 +0300

    master | minor changes

--- practice/base/base7_color.c
+++ practice/base/base7_color.c
@@ -2,14 +2,20 @@
  * Created by efreyu on 1/30/19.
  */
 #include <stdio.h>
+#include <unistd.h>
 #include "base7.h"
 
 int main(int argc, const char **argv) {
-    printf(ANSI_COLOR_RED     "This text is RED!"     ANSI_COLOR_RESET "\n");
-    printf(ANSI_COLOR_GREEN   "This text is GREEN!"   ANSI_COLOR_RESET "\n");
-    printf(ANSI_COLOR_YELLOW  "This text is YELLOW!"  ANSI_COLOR_RESET "\n");
-    printf(ANSI_COLOR_BLUE    "This text is BLUE!"    ANSI_COLOR_RESET "\n");
-    printf(ANSI_COLOR_MAGENTA "This text is MAGENTA!" ANSI_COLOR_RESET "\n");
-    printf(ANSI_COLOR_CYAN    "This text is CYAN!"    ANSI_COLOR_RESET "\n");
+    int i = 0;
+    while(i <= 10) {
+        i++;
+        usleep(200000);
+        printf(ANSI_COLOR_RED     "This text is RED!"     ANSI_COLOR_RESET "\n");
+        printf(ANSI_COLOR_GREEN   "This text is GREEN!"   ANSI_COLOR_RESET "\n");
+        printf(ANSI_COLOR_YELLOW  "This text is YELLOW!"  ANSI_COLOR_RESET "\n");
+        printf(ANSI_COLOR_BLUE    "This text is BLUE!"    ANSI_COLOR_RESET "\n");
+        printf(ANSI_COLOR_MAGENTA "This text is MAGENTA!" ANSI_COLOR_RESET "\n");
+        printf(ANSI_COLOR_CYAN    "This text is CYAN!"    ANSI_COLOR_RESET "\n");
+    }
     return 0;
 }
